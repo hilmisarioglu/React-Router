be4872cf7c9903c6cc450dcd1600683b0564f4dd
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-08-28 18:07:13 +0200

    2

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useParams, useLocation} from 'react-router-dom';
-const Post = () => {
+/* const Post = () => {
     const {id } = useParams();
     const item = new URLSearchParams(useLocation().search);
     return (
@@ -10,15 +10,15 @@ const Post = () => {
             <h2>Posttaki name:{item.get('second')} </h2>
         </div>
     )
-}
-/* const Post = () => {
+} */
+const Post = () => {
     const {id } = useParams();
     return (
         <div>
             <h2>Post id:{id} </h2>
         </div>
     )
-}  */
+} 
 /* const Post = ({ match }) => {
     return (
         <div>
